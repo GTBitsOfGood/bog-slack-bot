73b6c29fdd4cb872a3abac17e9bc2d17b44c94d9
Chris <chrisjobando@gmail.com>
2020-02-05 11:36:47 -0500

    Attendance bots added

--- attendance_bits.py
+++ attendance_bits.py
@@ -17,15 +17,22 @@ workbook = xlrd.open_workbook(file)
 sheet = workbook.sheet_by_index(0)
 
 people_attended = 0
+attended = []
+people_not_found = 0
 
 for row in range(1,sheet.nrows):
     name = sheet.cell_value(row,0).title().strip()
     db_data = collection.find_one({"name": name})
     if db_data is None:
         print(name)
+        people_not_found += 1
     else:
-        print("Not Updating Database Yet!")
-        # collection.find_one_and_update({'name': name}, {'$inc': {'bits': 2}})
+        attended.append(name)
         people_attended += 1
+
+if people_not_found == 0:
+    print("Test")
+    for name in attended:
+        # collection.find_one_and_update({'name': name}, {'$inc': {'bits': 2}})
 
 print("Updated attendance for " + str(people_attended) + " people!")
